cfe4517daf2f61dd0e14f133ab03dc3093e9c3bc
Oskar Wickström <oskar.wickstrom@gmail.com>
2017-12-04 07:32:11 +0100

    No color in diff

--- etc/check-formatting.sh
+++ etc/check-formatting.sh
@@ -9,5 +9,5 @@ HS_FILES="$(find codegen -name '*.hs')"
 
 for file in $HS_FILES ; do
   echo "Format checking $file"
-  diff --color -u <(cat $file | hindent -XPatternSynonyms | stylish-haskell) <(cat $file)
+  diff -u <(cat $file | hindent -XPatternSynonyms | stylish-haskell) <(cat $file)
 done
