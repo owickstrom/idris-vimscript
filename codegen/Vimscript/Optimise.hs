2b8051779c38d23212210819da789cd8d51c05dd
Soham Chowdhury <chow.soham@gmail.com>
2017-12-04 19:36:18 +0530

    Move arity-renamer into new Renamer module

--- codegen/Vimscript/Optimise.hs
+++ codegen/Vimscript/Optimise.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE LambdaCase #-}
-
 module Vimscript.Optimise
   ( transforms
   ) where
@@ -10,30 +8,10 @@ import qualified Data.Text                   as T
 import           Vimscript.AST
 import qualified Vimscript.Optimise.DCE      as DCE
 import qualified Vimscript.Optimise.TCO      as TCO
+import qualified Vimscript.Optimise.Renamer  as Renamer
 
 transforms :: Program -> Program
-transforms = insertHeader . TCO.runPass . DCE.runPass . arityRename
-
-arityRename :: Program -> Program
-arityRename (Program ss) = Program ss'
-  where
-    ss' = map (transform goStmt) (transformBi goExpr ss)
-    goStmt =
-      \case
-        Function sn ns bl -> Function (goSnLen ns sn) ns bl
-        Call sn es -> Call (goSn (length es) sn) es
-        x -> x
-    goExpr =
-      \case
-        Apply (Ref sn) es -> Apply (Ref (goSnLen es sn)) es
-        x -> x
-    goSnLen = goSn . length
-    goSn count =
-      \case
-        ScopedName Script n -> ScopedName Script (arityRenameBinding n count)
-        x -> x
-    arityRenameBinding (Name n) arity =
-      Name (n <> T.pack "_" <> T.pack (show arity))
+transforms = insertHeader . TCO.runPass . DCE.runPass . Renamer.runPass
 
 insertHeader :: Program -> Program
 insertHeader (Program ss) = Program (c : ss)
