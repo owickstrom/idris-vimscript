64e05dc070052c1778c5dbe30bfb2935bdc06a74
Soham Chowdhury <chow.soham@gmail.com>
2017-12-04 19:32:43 +0530

    Refactor arity-renamer

--- codegen/Vimscript/Optimise.hs
+++ codegen/Vimscript/Optimise.hs
@@ -20,21 +20,22 @@ arityRename (Program ss) = Program ss'
     ss' = map (transform goStmt) (transformBi goExpr ss)
     goStmt =
       \case
-        Function sn ns bl -> Function (goSn (length ns) sn) ns bl
+        Function sn ns bl -> Function (goSnLen ns sn) ns bl
         Call sn es -> Call (goSn (length es) sn) es
         x -> x
     goExpr =
       \case
-        Apply (Ref sn) es -> Apply (Ref (goSn (length es) sn)) es
+        Apply (Ref sn) es -> Apply (Ref (goSnLen es sn)) es
         x -> x
+    goSnLen = goSn . length
     goSn count =
       \case
-        ScopedName Script (Name n) ->
-          ScopedName Script (Name (n <> T.pack "_" <> T.pack (show count)))
+        ScopedName Script n -> ScopedName Script (arityRenameBinding n count)
         x -> x
+    arityRenameBinding (Name n) arity =
+      Name (n <> T.pack "_" <> T.pack (show arity))
 
 insertHeader :: Program -> Program
 insertHeader (Program ss) = Program (c : ss)
   where
     c = LineComment (T.pack "Generated from Idris")
-
